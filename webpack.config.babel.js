39b18ef37b3e8921f37a75eb8f6e155fee000f0e
Raman Amatya <ramanamatya@gmail.com>
2018-08-29 00:20:10 +0545

    webpack initialization.

---
+++ webpack.config.babel.js
@@ -0,0 +1,61 @@
+import webpack from 'webpack';
+import path from 'path';
+import CompressionPlugin from 'compression-webpack-plugin';
+
+const APP_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  entry: `${APP_DIR}/index.jsx`,
+  output: {
+    path: BUILD_DIR,
+    filename: env === 'production' ? '[name].[chunkhash].js' : '[name].js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(env),
+      },
+    }),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|html)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: [
+              'es2015',
+              'react',
+              'stage-0',
+            ],
+            plugins: [
+              'transform-class-properties',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
+
+export default config;
